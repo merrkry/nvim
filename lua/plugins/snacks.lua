bbf8fdacb9310caca91c0e31e0bf68c12ffa8712
merrkry <merrkry@tsubasa.moe>
2025-07-21 00:02:40 +0200

    plugins: add snacks.nvim

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			dashboard = {
+				enable = true,
+			},
+		},
+		keys = {
+		},
+	},
+}
