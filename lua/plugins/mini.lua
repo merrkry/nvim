f581e4b471211f184e921ea77fa9f94e2254f687
merrkry <merrkry@tsubasa.moe>
2025-07-20 22:51:16 +0200

    plugins: add mini.nvim

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		'echasnovski/mini.nvim',
+		version = false, -- change to '*' to use release branches
+		config = function()
+			require('mini.surround').setup(
+				{
+					-- from LazyVim
+					mappings = {
+						add = "gsa", -- Add surrounding in Normal and Visual modes
+						delete = "gsd", -- Delete surrounding
+						find = "gsf", -- Find surrounding (to the right)
+						find_left = "gsF", -- Find surrounding (to the left)
+						highlight = "gsh", -- Highlight surrounding
+						replace = "gsr", -- Replace surrounding
+						update_n_lines = "gsn", -- Update `n_lines`
+					},
+				}
+			)
+		end,
+	},
+}
