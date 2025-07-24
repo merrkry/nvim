58273ad784e784f0b533c859adde432178cf1705
merrkry <merrkry@tsubasa.moe>
2025-07-24 11:01:08 +0200

    autopari: migrate to ultimate-autopair

---
+++ lua/plugins/blink-pairs.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"saghen/blink.pairs",
+		version = "*", -- (recommended) only required with prebuilt binaries
+		dependencies = "saghen/blink.download",
+		--- @module 'blink.pairs'
+		--- @type blink.pairs.Config
+		opts = {},
+	},
+}
