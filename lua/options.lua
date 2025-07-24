cabbd77be63be941ecb0781786ed2e004cc25203
merrkry <merrkry@tsubasa.moe>
2025-07-24 16:43:44 +0200

    plugins: add colorful-winsep.nvim

--- lua/options.lua
+++ lua/options.lua
@@ -72,3 +72,5 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+vim.o.laststatus = 3
---
+++ lua/plugins/colorful-winsep.lua
@@ -0,0 +1,8 @@
+return {
+	{
+		"nvim-zh/colorful-winsep.nvim",
+		config = true,
+		event = { "WinLeave" },
+		opts = { hi = { fg = "#e5c07b" } },
+	},
+}
