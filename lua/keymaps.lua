fc4f70944e0ed237d9f1a19ac180fb54a6253952
merrkry <merrkry@tsubasa.moe>
2025-08-07 01:15:53 +0200

    keymap: separate vim register and system clipboard

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -79,3 +79,8 @@ vim.keymap.set("i", "<M-E>", "<C-o>E")
 vim.keymap.set("i", "<M-a>", "<C-o>0")
 vim.keymap.set("i", "<M-e>", "<C-o>$")
 vim.keymap.set("i", "<M-m>", "<C-o>_")
+
+-- system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste after curcor, from system clipboard" })
+vim.keymap.set("n", "<leader>P", '"+P', { desc = "Paste before curcor, from system clipboard" })
