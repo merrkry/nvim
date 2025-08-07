d0418d3801bf4b7d80c15ff6a767641d238a4bfa
merrkry <merrkry@tsubasa.moe>
2025-08-07 02:05:13 +0200

    treesitter: switch to nvim-treesitter/main

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,36 +1,32 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		-- https://github.com/OXY2DEV/markview.nvim?tab=readme-ov-file#-lazynvim
-		branch = "master", -- TODO: migrate to main
-		lazy = false,
+		branch = "main",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				auto_install = true,
-				highlight = {
-					enable = true,
-					-- FIXME: This doesn't take effect.
-					ensure_installed = {
-						"bash",
-						"diff",
-						"lua",
-						"luadoc",
-						"markdown",
-						"markdown_inline",
-						"query",
-						"regex",
-						"vim",
-						"vimdoc",
-					},
-					-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-					--  If you are experiencing weird indenting issues, add the language to
-					--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-					additional_vim_regex_highlighting = { "ruby" },
-				},
-				indent = { enable = true, disable = { "ruby" } },
-			})
-		end,
+	},
+	{
+		"MeanderingProgrammer/treesitter-modules.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			ensure_installed = {
+				"bash",
+				"comment",
+				"diff",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"regex",
+				"rust",
+				"vim",
+				"vimdoc",
+			},
+			auto_install = true,
+			fold = { enable = false },
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = { enable = false },
+		},
 	},
 }
