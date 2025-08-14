54c4ca4c756548fa3ec2af84818b072834887161
merrkry <merrkry@tsubasa.moe>
2025-08-14 16:06:59 +0200

    hardtime.nvim: no restricted keys

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -4,7 +4,21 @@ return {
 		event = "VeryLazy",
 		dependencies = { "MunifTanjim/nui.nvim" },
 		opts = {
+			notification = false,
 			disable_mouse = false,
+			-- https://github.com/m4xshen/hardtime.nvim/blob/6d7664d5bdfaea44c5f50b29f5239fab7b00c273/lua/hardtime/config.lua#L50-L61
+			restricted_keys = {
+				["h"] = false,
+				["j"] = false,
+				["k"] = false,
+				["l"] = false,
+				["+"] = false,
+				["gj"] = false,
+				["gk"] = false,
+				["<C-M>"] = false,
+				["<C-N>"] = false,
+				["<C-P>"] = false,
+			},
 		},
 	},
 }
