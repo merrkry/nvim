15e992ab746a1251b5c474e407e89c2668d42559
merrkry <merrkry@tsubasa.moe>
2025-08-14 23:22:24 +0200

    gitsigns: disable line blame
    horrible performance in nixpkgs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,9 +3,7 @@ return {
 		"lewis6991/gitsigns.nvim",
 		event = "VeryLazy",
 		config = function()
-			require("gitsigns").setup({
-				current_line_blame = true,
-			})
+			require("gitsigns").setup({})
 		end,
 	},
 }
