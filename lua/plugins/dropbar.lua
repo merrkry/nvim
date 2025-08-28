a3d015378913bdae31c5de608b9de1afab494ca0
merrkry <merrkry@tsubasa.moe>
2025-08-28 15:04:24 +0200

    dropbar: enable keybinds

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -18,10 +18,10 @@ return {
 			vim.api.nvim_set_hl(0, "WinBarNC", {
 				bg = "none",
 			})
-			-- local dropbar_api = require('dropbar.api')
-			-- vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-			-- vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-			-- vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+			local dropbar_api = require("dropbar.api")
+			vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
 		end,
 	},
 }
