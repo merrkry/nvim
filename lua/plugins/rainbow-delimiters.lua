a1eb1e0282f420a0aab7e837b52e1d7707d834f8
Merrkry <merrkry@tsubasa.moe>
2025-10-03 22:17:20 +0200

    lazy: bump

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,17 +1,19 @@
 return {
-	"HiPhish/rainbow-delimiters.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("rainbow-delimiters.setup").setup({
-			highlight = {
-				"RainbowDelimiterRed",
-				"RainbowDelimiterOrange",
-				"RainbowDelimiterYellow",
-				"RainbowDelimiterGreen",
-				"RainbowDelimiterCyan",
-				"RainbowDelimiterBlue",
-				"RainbowDelimiterViolet",
-			},
-		})
-	end,
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("rainbow-delimiters.setup").setup({
+				highlight = {
+					"RainbowDelimiterRed",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterCyan",
+					"RainbowDelimiterBlue",
+					"RainbowDelimiterViolet",
+				},
+			})
+		end,
+	},
 }
