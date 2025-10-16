88cf60e44b58e81eee2896b00a3ca4c3b5e83731
Merrkry <merrkry@tsubasa.moe>
2025-10-16 15:10:34 +0200

    completion: keymap adjustments

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,13 +1,9 @@
 return {
 	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
 	dependencies = { "rafamadriz/friendly-snippets" },
 
-	-- use a release tag to download pre-built binaries
 	-- version = "1.*",
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
 	build = "nix run .#build-plugin",
 
 	---@module 'blink.cmp'
@@ -21,7 +17,7 @@ return {
 
 			["<C-y>"] = { "accept", "fallback" },
 			-- ["<CR>"] = { "accept", "fallback" },
-			-- ["<Tab>"] = { "select_and_accept", "fallback" },
+			["<Tab>"] = { "select_and_accept", "fallback" },
 
 			["<Up>"] = { "select_prev", "fallback" },
 			["<Down>"] = { "select_next", "fallback" },
@@ -38,12 +34,9 @@ return {
 		},
 
 		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "mono",
 		},
 
-		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
 			list = {
 				selection = {
@@ -53,14 +46,13 @@ return {
 			},
 			documentation = {
 				auto_show = true,
-				auto_show_delay_ms = 0,
-				--
-				-- window = { border = "single" },
+				auto_show_delay_ms = 1000,
+				window = { border = vim.o.winborder },
 			},
 
 			-- colorful-menu.nvim
 			menu = {
-				-- border = "single",
+				border = vim.o.winborder,
 				draw = {
 					-- We don't need label_description now because label and label_description are already
 					-- combined together in label by colorful-menu.nvim.
@@ -85,11 +77,6 @@ return {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
 
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
 		fuzzy = {
 			implementation = "prefer_rust_with_warning",
 			sorts = {
