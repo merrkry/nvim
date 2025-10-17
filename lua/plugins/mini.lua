ff6a899f56c5ba0af19ae0941059481818a7b5eb
Merrkry <merrkry@tsubasa.moe>
2025-10-17 21:46:17 +0200

    keymap: adjustments

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,18 +11,7 @@ return {
 
 			require("mini.splitjoin").setup()
 
-			require("mini.surround").setup({
-				-- from LazyVim
-				mappings = {
-					add = "<leader>sa", -- Add surrounding in Normal and Visual modes
-					delete = "<leader>sd", -- Delete surrounding
-					find = "<leader>sf", -- Find surrounding (to the right)
-					find_left = "<leader>sF", -- Find surrounding (to the left)
-					highlight = "<leader>sh", -- Highlight surrounding
-					replace = "<leader>sr", -- Replace surrounding
-					update_n_lines = "<leader>sn", -- Update `n_lines`
-				},
-			})
+			require("mini.surround").setup()
 
 			require("mini.bracketed").setup()
 
