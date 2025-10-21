ee94d18369d3d99b234d5fac73987084173eabb5
Merrkry <merrkry@tsubasa.moe>
2025-10-21 14:08:38 +0200

    undotree: audo diff

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"mbbill/undotree",
 		config = function()
-			vim.g.undotree_DiffAutoOpen = 0
+			-- vim.g.undotree_DiffAutoOpen = 0
 		end,
 		keys = {
 			{ "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle undotree panel" },
