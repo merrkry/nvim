d4295123b1f935ab695948456c201daa8dd46bf3
Merrkry <merrkry@tsubasa.moe>
2025-10-31 17:58:18 +0100

    atone.nvim: init, replace undotree

---
+++ lua/plugins/atone.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"XXiaoA/atone.nvim",
+		cmd = "Atone",
+		opts = {
+			layout = {
+				width = 0.2,
+			},
+		},
+		keys = {
+			{ "<leader>u", "<cmd>Aton toggle<cr>", desc = "Open undotree" },
+		},
+	},
+}
